d61a213c9eaf983d03d6a746934eaf9f746c02b9
Moses Kippe <53339961+mavrk-mose@users.noreply.github.com>
2024-12-10 13:18:31 +0000

    feat: added posthog for user analytics

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,15 +3,32 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
+import { PostHogProvider } from 'posthog-js/react';
+import { usePostHogPageTracking } from '@/hooks/use-post-hog-tracking';
+import posthog from 'posthog-js'
+import { useEffect } from 'react';
 
 const queryClient = new QueryClient();
 
+
 export function Providers({ children, ...props }: ThemeProviderProps) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      person_profiles: 'always',
+      debug: false
+    })
+  }, [])
+
+  usePostHogPageTracking();
+
   return (
     <QueryClientProvider client={queryClient}>
-      <NextThemesProvider {...props}>
-        {children}
-      </NextThemesProvider>
+      <PostHogProvider client={posthog}>
+        <NextThemesProvider {...props}>
+          {children}
+        </NextThemesProvider>
+      </PostHogProvider>
     </QueryClientProvider>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*', // Proxy to PostHog cloud service
+      },
+    ];
+  },
   experimental: {
     staleTimes: {
       dynamic: 30,
